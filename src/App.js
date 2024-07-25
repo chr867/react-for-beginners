8692ec00697d9415366ef124809992615e61a30c
최형로 <chr867423@gmail.com>
2024-07-26 03:18:52 +0900

    Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(()=>{
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response=>response.json())
+    .then(json => {
+      setCoins(json);
+      setLoading(false);
+    });
+  },[]);
+
+  const [money, setMoney] = useState(0);
+  const onChange = (evt) =>{
+    setMoney(evt.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Coins! ({coins.length})</h1>
+      <input type="number" value={money} onChange={onChange} placeholder="insert your money"></input>
+      {loading ?<strong>Loading...</strong> :<select>
+        {coins.map(item=>(
+          <option>
+            {item.name} ({item.symbol}): {money!=0 ?money/item.quotes.USD.price :item.quotes.USD.price} USD
+          </option>
+        ))}
+      </select>}
+
     </div>
   );
+
 }
 
 export default App;
